715ea56e03ac62de0637e932e6d6f6e8ff025301
Mandy Yeung <mandycodes@gmail.com>
2015-01-03 11:14:02 -0500

    Add status labels

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,34 @@
 $(document).ready(function(){
-  $('#results th a').on("click", function(){
-    $.getScript(this.href);
-    return false;
+  // $('#results th a').on("click", function(){
+  //   $.getScript(this.href);
+  //   return false;
+  // });
+  $('.label').each(function(){
+    var $this = $(this),
+        classToAdd = null;
+    switch ($this.text()){
+      case 'Draft':
+        classToAdd = 'label-info';
+        break;
+      case 'Open':
+        classToAdd = 'label-success';
+        break;
+      case 'Closed':
+        classToAdd = 'label-primary';
+        break;
+      case 'Re-Opened':
+        classToAdd = 'label-warning';
+        break;
+      case 'Void':
+        classToAdd = 'label-default';
+        break;
+      case 'Field Impact':
+        classToAdd = 'label-danger';
+        break;
+    }
+    if (classToAdd !== null){
+      $this.addClass(classToAdd);
+    }
   });
+
 });
