1f79d44548e03558fdb548191dbc75fe5f7ab696
Mandy Yeung <mandycodes@gmail.com>
2015-01-03 18:33:31 -0500

    Add jQuery turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap-sprockets
 //= require local_time
 //= require_tree .
+//= require turbolinks
