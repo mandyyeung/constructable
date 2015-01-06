9a12271fd9bc28b6bba64fa7094a6fbde96367be
Mandy Yeung <mandycodes@gmail.com>
2015-01-05 21:11:27 -0500

    Completed donut and bar charts

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,44 +1,35 @@
 // Dashboard donut
 
 Morris.Donut({
-  element: 'donut',
-  data: [
-    {label: "Increase", value: 12},
-    {label: "Decrease", value: 30},
-    {label: "Unknown", value: 20}
-  ],
+  element: 'cost_donut',
+  data: $('#cost_donut').data('costs'),
   resize: true,
   hideHover: 'auto',
   colors: ["#933B39","#C05F5D","#D18B89"]
 });
 
-// Dashboard line
-
-Morris.Line({
-  element: 'dates-line',
-  data: $('#dates-line').data('dates'),
-  xkey: 'created_at',
-  ykeys: ['id'],
-  labels: ['Id'],
-  hideHover: 'auto',
-  behaveLikeLine: true,
-  resize: true
-})
-
 // Dashboard bar
 
 Morris.Bar({
-  element: 'bar',
-  data: [
-    { y: 'Core & Shell', a: 75 },
-    { y: 'MEP', a: 50 },
-    { y: 'Interiors', a: 75 },
-    { y: 'Site', a: 100 }
-  ],
-  xkey: 'y',
-  ykeys: ['a'],
+  element: 'trade_bar',
+  data: $('#trade_bar').data('trades'),
+  xkey: 'x',
+  ykeys: ['y'],
   xLabelMargin: 10,
-  labels: ['Total'],
+  labels: ['RFI Count'],
   hideHover: 'auto',
+  resize: true
+});
+
+// Dashboard line
+
+Morris.Line({
+  element: 'dates_line',
+  data: $('#dates_line').data('dates'),
+  xkey: 'x',
+  ykeys: ['y'],
+  labels: ['# of RFIs'],
+  hideHover: 'auto',
+  behaveLikeLine: true,
   resize: true
 });
--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -7,4 +7,24 @@ module RequestsHelper
       }
     end
   end
+
+  def cost_donut_data
+    # this method could be refactored to account for futher changes to cost_impact options
+    options = [{label: "Increase"}, {label: "Decrease"}, {label: "Unknown"}]
+    options.map do |option|
+      count = Request.where("cost_impact = ?", option[:label]).count
+      option[:value] = count
+    end
+    options
+  end
+
+  def trade_bar_data
+    # this method could be refactored to account for futher changes to trade options
+    options = [{x: "Core & Shell"}, {x: "MEP"}, {x: "Interiors"}, {x: "Sitework"}]
+    options.map do |option|
+      count = Request.where("trade = ?", option[:x]).count
+      option[:y] = count
+    end
+    options
+  end
 end
