cbf155b5e092c8cb289cdd76df23251073d2a34a
Mandy Yeung <mandycodes@gmail.com>
2015-01-11 14:11:29 -0500

    Update latest activities on header nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_latest_activities
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:full_name, :admin, :category, :avatar]
     devise_parameter_sanitizer.for(:account_update) << [:full_name, :admin, :category, :avatar]
+  end
+
+  def set_latest_activities
+    @latest_activities = PublicActivity::Activity.order("created_at desc").where(owner_id: User.all.ids - [current_user.id], owner_type: 'User').take(3)
   end
 end
