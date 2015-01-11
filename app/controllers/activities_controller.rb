60a1814ed21a7e2b98a7fb8ef19b414b43496ab7
Mandy Yeung <mandycodes@gmail.com>
2015-01-11 13:35:10 -0500

    Include latest activities on header nav bar

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,10 +2,11 @@ class ActivitiesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: User.all.ids - [current_user.id], owner_type: 'User').paginate(page: params[:page], per_page: 3)
+    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: User.all.ids - [current_user.id], owner_type: 'User').paginate(page: params[:page], per_page: 5)
     respond_to do |format|
       format.html
       format.js
     end
   end
+
 end
