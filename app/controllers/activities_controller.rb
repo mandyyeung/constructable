b1b94bcc30e04c149ec67d72248392f67f2726f6
Mandy Yeung <mandycodes@gmail.com>
2015-01-12 22:37:28 -0500

    Update About page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: User.all.ids - [current_user.id], owner_type: 'User').paginate(page: params[:page], per_page: 5)
+    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: User.all.ids - [current_user.id], owner_type: 'User').paginate(page: params[:page], per_page: 20)
     respond_to do |format|
       format.html
       format.js
